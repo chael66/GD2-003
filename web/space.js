b8720601f85ed2d4a6694178bf669e15d8f4a8b3
Lim <chullchul66@gmail.com>
2021-03-03 05:10:41 +0900

    11

---
+++ web/space.js
@@ -0,0 +1,12 @@
+
+
+$(document).ready(function() {
+    $('.testbt1').click(function(){
+      $('#test').animate({opacity:"0"}, 1000);
+    });
+    
+    $('.testbt2').click(function(){
+      $('#test').animate({opacity:"1"}, 1000);
+    });
+  })
+  
---
+++ web/space1.js
@@ -0,0 +1,9 @@
+
+
+
+
+$('#toggle').click(function() {
+    $('#toggle .bar').toggleClass('animate');
+    $('#page').toggleClass('overlay');
+  });
+
